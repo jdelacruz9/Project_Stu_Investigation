4d57b24475196ad4c7e2a859784e89632cb1b10d
danielram <bojangles7856@gmail.com>
2014-05-06 03:12:35 -0400

    what is dis

--- signin.php
+++ signin.php
@@ -14,7 +14,7 @@ mysql_select_DB($database);
 session_start();
 if(strlen($_SESSION['pass']) > 20 or strlen($_SESSION['user']) > 20)
 {
-header("location: http://ada.uprrp.edu/~dramirez2/ccom4027/project/login.html");
+header("location: http://ada.uprrp.edu/~ijimenez/Estu_Investigar/login.html");
 }
 else
 {
@@ -23,11 +23,11 @@ $query_pass = mysql_query('select pass from Passwords where username = "'.$_SESS
 
 if (/*mysql_num_rows($query_pass) > 0 or*/ mysql_num_rows($query_users) > 0) // More than 1 row returned which means there is data
 {
-header("location: http://ada.uprrp.edu/~dramirez2/ccom4027/project/investiga.php"); //ALERT!!! something typed is wrong; try again
+//header("location: http://ada.uprrp.edu/~dramirez2/ccom4027/project/investiga.php"); //ALERT!!! something typed is wrong; try again
 
 }else{ // No rows were returned therefore there were no matches
 
-header("location: http://ada.uprrp.edu/~dramirez2/ccom4027/project/login.html"); //jump to PORTADA: investiga.php
+header("location: http://ada.uprrp.edu/~ijimenez/Estu_Investigar/login.html"); //jump to PORTADA: investiga.php
 }
 }
 ?>
