8299f15a9eedec072cad6a4b00dd0870802818d2
Ivan Jimenez <ivanjimenezruiz@yahoo.com>
2014-05-06 01:38:23 -0400

    Fixed Login pages

--- login.php
+++ login.php
@@ -1,22 +1,8 @@
 <?php
-	$host = "localhost";
-	$usuario = "dramirez2";
-	$password = "turntablepower2";
-	$database = "estu_investigacion";
+	session_start();
+	$_SESSION['user']=$_REQUEST['usernames'];
+	$_SESSION['pass']=$_REQUEST['passwords'];
 
-	$conexion = mysql_connect($host, $usuario, $password);
-	mysql_select_DB($database);
+	header("location: http://ada.uprrp.edu/~ijimenez/Estu_Investigar/signin.php")
 	
-	$user = $_POST['usernames'];
-	$pass = $_POST['passwords'];
-	
-	$sql_users = 'select username from Passwords where pass = "'.$pass.'";';
-	$query_users = mysql_query($sql_users);
-	$sql_pass = 'select pass from Passwords where username = "'.$user.'";';
-	$query_pass = mysql_query($sql_pass);
-	if ($query_users == NULL && $query_ == NULL)
-		header("location: http://ada.uprrp.edu/~dramirez2/ccom4027/project/login.html"); //ALERT!!! something typed is wrong; try again
-	else 
-		header("location: http://ada.uprrp.edu/~dramirez2/ccom4027/project/investiga.php"); //jump to PORTADA: investiga.php
-
 ?>
